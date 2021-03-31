2c1163c95a6617dccc44927f3d8b26d894184c85
Jamie Sharpe <github@jamiesharpe.co.uk>
2021-03-31 11:00:00 +0100

    Refactored ilapfuncs.py.
    * removed unused python module.
    * double spaces between function/class declarations. - PEP 8
    * Doc strings to triple double quotes. - PEP 257
    * Removed unnecessary string conversion. strftime returns a string.
    * During folder creation, fail silently if they already exist.
    * Simplified new folder name generator.
    * Moved magic string into variable.
    * Restructured if condition chain.
    * Moved string variables outside of try block to focus the exception handling on the database access.
    * f-strings implicitly convert 'ex' to string.
    * Moved column name checking outside of try block for same reason above.
    * Moved string variables outside of try block for same reason above.
    * Refactored column name detection using row count.
    * Moved print call outside of the context manager. If the open call fails, the message will not be printed for debugging.
    * Commented out unused function instead of a floating literal string.
    * Fail silently if the folder already exists during creation.
    * Skip iteration logic if file name doesn't match criteria.
    * Removed unused variables 'head', 'tail', and 'output_file_rows'.
    * Fixed over indentation.
    * Using Python3 supports encoding with 'open()', so the 'codecs.open' is no longer needed.
    * 'rstrip' works on all characters within the string individually.
    * Fail silently on folder creation if it already exists.
    * Utilise 'writerows' instead of iterating through the list.
    * Moved variables out of if blocks [repetitive].
    * Set database access to 'exclusive' with intention to write large amounts of data.
    * Change while loop into iterator loop.
    * Moved commit to end of if/else.
    * Removed unused parameter 'open = 1'.
    * Converted while loop to item iteration.
    * Added dictionary key checks.
    * Added Null Island and None timestamp check.
    * Added return type indicator.

--- scripts/ilapfuncs.py
+++ scripts/ilapfuncs.py
@@ -1,4 +1,3 @@
-import codecs
 import csv
 import datetime
 import os
@@ -8,101 +7,119 @@ import simplekml
 
 from bs4 import BeautifulSoup
 
+
 class OutputParameters:
-    '''Defines the parameters that are common for '''
+    """Defines the parameters that are common for """
     # static parameters
     nl = '\n'
     screen_output_file_path = ''
     
     def __init__(self, output_folder):
         now = datetime.datetime.now()
-        currenttime = str(now.strftime('%Y-%m-%d_%A_%H%M%S'))
+        currenttime = now.strftime('%Y-%m-%d_%A_%H%M%S')
         self.report_folder_base = os.path.join(output_folder, 'ALEAPP_Reports_' + currenttime) # aleapp , aleappGUI, ileap_artifacts, report.py
         self.temp_folder = os.path.join(self.report_folder_base, 'temp')
         OutputParameters.screen_output_file_path = os.path.join(self.report_folder_base, 'Script Logs', 'Screen Output.html')
 
-        os.makedirs(os.path.join(self.report_folder_base, 'Script Logs'))
-        os.makedirs(self.temp_folder)
+        os.makedirs(os.path.join(self.report_folder_base, 'Script Logs'), exist_ok = True)
+        os.makedirs(self.temp_folder, exist_ok = True)
+
 
 def is_platform_windows():
-    '''Returns True if running on Windows'''
+    """Returns True if running on Windows"""
     return os.name == 'nt'
 
+
 def sanitize_file_path(filename, replacement_char='_'):
-    '''
+    """
     Removes illegal characters (for windows) from the string passed. Does not replace \ or /
-    '''
+    """
     return re.sub(r'[*?:"<>|\'\r\n]', replacement_char, filename)
 
+
 def sanitize_file_name(filename, replacement_char='_'):
-    '''
+    """
     Removes illegal characters (for windows) from the string passed.
-    '''
+    """
     return re.sub(r'[\\/*?:"<>|\'\r\n]', replacement_char, filename)
 
+
 def get_next_unused_name(path):
-    '''Checks if path exists, if it does, finds an unused name by appending -xx
+    """Checks if path exists, if it does, finds an unused name by appending -xx
        where xx=00-99. Return value is new path.
        If it is a file like abc.txt, then abc-01.txt will be the next
-    '''
-    folder, basename = os.path.split(path)
-    ext = None
-    if basename.find('.') > 0:
-        basename, ext = os.path.splitext(basename)
+    """
+
+    basename, ext = os.path.splitext(path)
+
     num = 1
-    new_name = basename
-    if ext is not None:
-        new_name += f"{ext}"
-    while os.path.exists(os.path.join(folder, new_name)):
-        new_name = basename + "-{:02}".format(num)
-        if ext is not None:
-            new_name += f"{ext}"
+    found_new_file_name = False
+
+    while not found_new_file_name:
+        new_file_path = f'{basename}-{num:02}{ext}'
+        if not os.path.exists(new_file_path):
+            found_new_file_name = True
         num += 1
-    return os.path.join(folder, new_name)
+
+    return new_file_path
+
 
 def open_sqlite_db_readonly(path):
-    '''Opens an sqlite db in read-only mode, so original db (and -wal/journal are intact)'''
+    """Opens an sqlite db in read-only mode, so original db (and -wal/journal are intact)"""
     if is_platform_windows():
-        if path.startswith('\\\\?\\UNC\\'): # UNC long path
-            path = "%5C%5C%3F%5C" + path[4:]
-        elif path.startswith('\\\\?\\'):    # normal long path
-            path = "%5C%5C%3F%5C" + path[4:]
+
+        url_encoded_prefix = '%5C%5C%3F%5C'  # URL escaped \\?\
+
+        if (
+            path.startswith('\\\\?\\UNC\\') or  # UNC long path
+            path.startswith('\\\\?\\')  # normal long path
+        ):
+            path = url_encoded_prefix + path[4:]
         elif path.startswith('\\\\'):       # UNC path
-            path = "%5C%5C%3F%5C\\UNC" + path[1:]
+            path = f'{url_encoded_prefix}\\UNC{path[1:]}'
         else:                               # normal path
-            path = "%5C%5C%3F%5C" + path
-    return sqlite3.connect (f"file:{path}?mode=ro", uri=True)
+            path = f'{url_encoded_prefix}{path}'
+    return sqlite3.connect(f'file:{path}?mode=ro', uri=True)
+
 
 def does_column_exist_in_db(db, table_name, col_name):
-    '''Checks if a specific col exists'''
+    """Checks if a specific col exists"""
     col_name = col_name.lower()
+    query = f"pragma table_info('{table_name}');"
+    all_rows = []
+
     try:
         db.row_factory = sqlite3.Row # For fetching columns by name
-        query = f"pragma table_info('{table_name}');"
         cursor = db.cursor()
         cursor.execute(query)
         all_rows = cursor.fetchall()
-        for row in all_rows:
-            if row['name'].lower() == col_name:
-                return True
     except sqlite3.Error as ex:
-        print(f"Query error, query={query} Error={str(ex)}")
-        pass
+        print(f'Query error, query={query} Error={ex}')
+
+    for row in all_rows:
+        if row['name'].lower() == col_name:
+            return True
     return False
+
 
 def does_table_exist(db, table_name):
-    '''Checks if a table with specified name exists in an sqlite db'''
+    """Checks if a table with specified name exists in an sqlite db"""
+    query = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'"
+    row_count = 0
+
     try:
-        query = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'"
         cursor = db.execute(query)
-        for row in cursor:
-            return True
-    except sqlite3Error as ex:
-        logfunc(f"Query error, query={query} Error={str(ex)}")
+        row_count = cursor.rowcount()
+    except sqlite3.Error as ex:
+        logfunc(f'Query error, query={query} Error={ex}')
+
+    if row_count >= 1:
+        return True
     return False
 
+
 class GuiWindow:
-    '''This only exists to hold window handle if script is run from GUI'''
+    """This only exists to hold window handle if script is run from GUI"""
     window_handle = None # static variable 
     progress_bar_total = 0
     progress_bar_handle = None
@@ -112,160 +129,176 @@ class GuiWindow:
         if GuiWindow.progress_bar_handle:
             GuiWindow.progress_bar_handle.UpdateBar(n)
 
-def logfunc(message=""):
+
+def logfunc(message: str = ''):
+    """
+    Prints and writes a log message.
+    Calls the GUI to be refreshed.
+    :param message: Message to print.
+    :return None:
+    """
+    print(message)
     with open(OutputParameters.screen_output_file_path, 'a', encoding='utf8') as a:
-        print(message)
         a.write(message + '<br>' + OutputParameters.nl)
 
     if GuiWindow.window_handle:
         GuiWindow.window_handle.refresh()
-    
-""" def deviceinfoin(ordes, kas, vas, sources): # unused function
-    sources = str(sources)
-    db = sqlite3.connect(reportfolderbase+'Device Info/di.db')
-    cursor = db.cursor()
-    datainsert = (ordes, kas, vas, sources,)
-    cursor.execute('INSERT INTO devinf (ord, ka, va, source)  VALUES(?,?,?,?)', datainsert)
-    db.commit() """
-    
+
+
+# def deviceinfoin(ordes, kas, vas, sources): # unused function
+#     sources = str(sources)
+#     db = sqlite3.connect(reportfolderbase+'Device Info/di.db')
+#     cursor = db.cursor()
+#     datainsert = (ordes, kas, vas, sources,)
+#     cursor.execute('INSERT INTO devinf (ord, ka, va, source)  VALUES(?,?,?,?)', datainsert)
+#     db.commit()
+
+
 def html2csv(reportfolderbase):
-    #List of items that take too long to convert or that shouldn't be converted
+    """
+    TODO: Consider removing, unused function.
+    :param reportfolderbase: path to html files.
+    :return:
+    """
+
+    # List of items that take too long to convert or that shouldn't be converted
     itemstoignore = ['index.html',
                     'Distribution Keys.html', 
                     'StrucMetadata.html',
                     'StrucMetadataCombined.html']
-                    
-    if os.path.isdir(os.path.join(reportfolderbase, '_CSV Exports')):
-        pass
-    else:
-        os.makedirs(os.path.join(reportfolderbase, '_CSV Exports'))
+
+    path_csv_files = os.path.join(reportfolderbase, '_CSV Exports')
+
+    os.makedirs(path_csv_files, exist_ok = True)
+
     for root, dirs, files in sorted(os.walk(reportfolderbase)):
         for file in files:
-            if file.endswith(".html"):
-                fullpath = (os.path.join(root, file))
-                head, tail = os.path.split(fullpath)
-                if file in itemstoignore:
-                    pass
-                else:
-                    data = open(fullpath, 'r', encoding='utf8')
-                    soup=BeautifulSoup(data,'html.parser')
-                    tables = soup.find_all("table")
-                    data.close()
-                    output_final_rows=[]
+            if not file.endswith('.html') or file in itemstoignore:
+                continue
 
-                    for table in tables:
-                        output_rows = []
-                        for table_row in table.findAll('tr'):
+            fullpath = (os.path.join(root, file))
+            data = open(fullpath, 'r', encoding='utf8')
+            soup = BeautifulSoup(data, 'html.parser')
+            tables = soup.find_all('table')
+            data.close()
 
-                            columns = table_row.findAll('td')
-                            output_row = []
-                            for column in columns:
-                                    output_row.append(column.text)
-                            output_rows.append(output_row)
-        
-                        file = (os.path.splitext(file)[0])
-                        with codecs.open(os.path.join(reportfolderbase, '_CSV Exports',  file +'.csv'), 'a', 'utf-8-sig') as csvfile:
-                            writer = csv.writer(csvfile, quotechar='"', quoting=csv.QUOTE_ALL)
-                            writer.writerows(output_rows)
+            for table in tables:
+                output_rows = []
+                for table_row in table.findAll('tr'):
+
+                    columns = table_row.findAll('td')
+                    output_row = []
+                    for column in columns:
+                        output_row.append(column.text)
+                    output_rows.append(output_row)
+
+                file = os.path.splitext(file)[0]
+                with open(os.path.join(path_csv_files,  file + '.csv'), mode='a', encoding='utf-8-sig') as csvfile:
+                    writer = csv.writer(csvfile, quotechar='"', quoting=csv.QUOTE_ALL)
+                    writer.writerows(output_rows)
+
 
 def tsv(report_folder, data_headers, data_list, tsvname):
-    report_folder = report_folder.rstrip('/')
-    report_folder = report_folder.rstrip('\\')
+    report_folder = report_folder.rstrip('/\\')
     report_folder_base, tail = os.path.split(report_folder)
     tsv_report_folder = os.path.join(report_folder_base, '_TSV Exports')
-    
-    if os.path.isdir(tsv_report_folder):
-        pass
-    else:
-        os.makedirs(tsv_report_folder)
 
-    with codecs.open(os.path.join(tsv_report_folder, tsvname +'.tsv'), 'a', 'utf-8-sig') as tsvfile:
+    os.makedirs(tsv_report_folder, exist_ok = True)
+
+    with open(os.path.join(tsv_report_folder, tsvname + '.tsv'), mode='a', encoding='utf-8-sig') as tsvfile:
         tsv_writer = csv.writer(tsvfile, delimiter='\t')
         tsv_writer.writerow(data_headers)
-        for i in data_list:
-            tsv_writer.writerow(i)
+        tsv_writer.writerows(data_list)
+
 
 def timeline(report_folder, tlactivity, data_list, data_headers):
-    report_folder = report_folder.rstrip('/')
-    report_folder = report_folder.rstrip('\\')
+    """
+    Generates a timeline of events.
+
+    TODO: Wrap the function in a try/catch.
+
+    :param report_folder:
+    :param tlactivity:
+    :param data_list:
+    :param data_headers:
+    :return:
+    """
+
+    report_folder = report_folder.rstrip('/\\')
     report_folder_base, tail = os.path.split(report_folder)
     tl_report_folder = os.path.join(report_folder_base, '_Timeline')
 
-    if os.path.isdir(tl_report_folder):
-        tldb = os.path.join(tl_report_folder, 'tl.db')
-        db = sqlite3.connect(tldb)
+    os.makedirs(tl_report_folder, exist_ok = True)
+    tldb = os.path.join(tl_report_folder, 'tl.db')
+
+    if os.path.isfile(tldb):
+        db = sqlite3.connect(tldb, isolation_level = 'exclusive')
         cursor = db.cursor()
         cursor.execute('''PRAGMA synchronous = EXTRA''')
         cursor.execute('''PRAGMA journal_mode = WAL''')
     else:
-        os.makedirs(tl_report_folder)
-        #create database
-        tldb = os.path.join(tl_report_folder, 'tl.db')
         db = sqlite3.connect(tldb, isolation_level = 'exclusive')
         cursor = db.cursor()
         cursor.execute(
-        """
-        CREATE TABLE data(key TEXT, activity TEXT, datalist TEXT)
-        """
-            )
-        db.commit()
-    
-    a = 0
-    length = (len(data_list))
-    while a < length: 
-        modifiedList = list(map(lambda x, y: x + ': ' +  str(y), data_headers, data_list[a]))
-        cursor.executemany("INSERT INTO data VALUES(?,?,?)", [(str(data_list[a][0]), tlactivity, str(modifiedList))])
-        a += 1
+            """
+                CREATE TABLE data(key TEXT, activity TEXT, datalist TEXT)
+            """
+        )
+    db.commit()
+
+    # TODO: format the data to correctly capitalise on the cursor.executemany method.
+    # In the current state, this section of code is running a single insert query, many times, on a list containing a
+    # single item. It is not taking advantage of the executemany capabilities where a large list can be provided.
+    for data_item in data_list:
+        modifiedList = list(map(lambda x, y: x + ': ' + str(y), data_headers, data_item))
+        cursor.executemany("INSERT INTO data VALUES(?,?,?)", [(str(data_item[0]), tlactivity, str(modifiedList))])
     db.commit()
     db.close()
-    
+
+
 def kmlgen(report_folder, kmlactivity, data_list, data_headers):
-    report_folder = report_folder.rstrip('/')
-    report_folder = report_folder.rstrip('\\')
+    report_folder = report_folder.rstrip('/\\')
     report_folder_base, tail = os.path.split(report_folder)
     kml_report_folder = os.path.join(report_folder_base, '_KML Exports')
     
-    if os.path.isdir(kml_report_folder):
-        latlongdb = os.path.join(kml_report_folder, '_latlong.db')
+    os.makedirs(kml_report_folder, exist_ok = True)
+    latlongdb = os.path.join(kml_report_folder, '_latlong.db')
+
+    if os.path.isfile(latlongdb):
         db = sqlite3.connect(latlongdb)
         cursor = db.cursor()
         cursor.execute('''PRAGMA synchronous = EXTRA''')
         cursor.execute('''PRAGMA journal_mode = WAL''')
-        db.commit()
     else:
-        os.makedirs(kml_report_folder)
-        latlongdb = os.path.join(kml_report_folder, '_latlong.db')
         db = sqlite3.connect(latlongdb)
         cursor = db.cursor()
         cursor.execute(
-        """
-        CREATE TABLE data(key TEXT, latitude TEXT, longitude TEXT, activity TEXT)
-        """
-            )
-        db.commit()
-    
-    kml = simplekml.Kml(open=1)
-    
-    a = 0
-    length = (len(data_list))
-    while a < length:
-        modifiedDict = dict(zip(data_headers, data_list[a]))
-        times = modifiedDict['Timestamp']
-        lon = modifiedDict['Longitude']
-        lat = modifiedDict['Latitude']
-        if lat:
+            """
+                CREATE TABLE data(key TEXT, latitude TEXT, longitude TEXT, activity TEXT)
+            """
+        )
+    db.commit()
+
+    kml = simplekml.Kml()
+
+    for data_item in data_list:
+        modifiedDict = dict(zip(data_headers, data_item))
+        times = modifiedDict.get('Timestamp', None)
+        lon = modifiedDict.get('Longitude', 0)
+        lat = modifiedDict.get('Latitude', 0)
+        if (lat == 0 and lon == 0) and (times is not None):
             pnt = kml.newpoint()
             pnt.name = times
-            pnt.description = f"Timestamp: {times} - {kmlactivity}"
+            pnt.description = f'Timestamp: {times} - {kmlactivity}'
             pnt.coords = [(lon, lat)]
             cursor.execute("INSERT INTO data VALUES(?,?,?,?)", (times, lat, lon, kmlactivity))
-        a += 1
+
     db.commit()
     db.close()
     kml.save(os.path.join(kml_report_folder, f'{kmlactivity}.kml'))
 
 
-def convert_to_long_path(windows_path: str):
+def convert_to_long_path(windows_path: str) -> str:
     """
     In Windows, path lengths have a limitation of 260 characters.
     This can be expanded to 32,767 characters by prepending the path with "\\?\"
