ba09acf26780edbfddea3fd042a5b59bf5e62ede
Jamie Sharpe <github@jamiesharpe.co.uk>
2021-03-31 11:00:00 +0100

    Converted accounts_de.py to new plugin system.

--- scripts/artifacts/accounts_de.py
+++ scripts/artifacts/accounts_de.py
@@ -1,63 +1,79 @@
-import glob
-import json
-import os
-import shutil
-import sqlite3
-
+from scripts.ilapfuncs import timeline, is_platform_windows, open_sqlite_db_readonly
+from scripts.plugin_base import ArtefactPlugin
 from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows, open_sqlite_db_readonly
+from scripts.ilapfuncs import logfunc, tsv
 
-def get_accounts_de(files_found, report_folder, seeker, wrap_text):
 
-    slash = '\\' if is_platform_windows() else '/' 
+class AccountsDePlugin(ArtefactPlugin):
+    """
+    """
 
-    # Filter for path xxx/yyy/system_ce/0
-    for file_found in files_found:
-        file_found = str(file_found)
-        parts = file_found.split(slash)
-        uid = parts[-2]
-        try:
-            uid_int = int(uid)
-            # Skip sbin/.magisk/mirror/data/system_de/0 , it should be duplicate data??
-            if file_found.find('{0}mirror{0}'.format(slash)) >= 0:
-                continue
-            process_accounts_de(file_found, uid, report_folder)
-        except ValueError:
-                pass # uid was not a number
+    def __init__(self):
+        super().__init__()
+        self.author = 'Unknown'
+        self.author_email = ''
+        self.author_url = ''
 
-def process_accounts_de(folder, uid, report_folder):
-    
-    #Query to create report
-    db = open_sqlite_db_readonly(folder)
-    cursor = db.cursor()
+        self.name = 'Accounts_de'
+        self.description = ''
 
-    #Query to create report
-    cursor.execute('''
-    SELECT
-        datetime(last_password_entry_time_millis_epoch / 1000, 'unixepoch') as 'last pass entry',
-        name,
-        type
-        FROM
-    accounts
-    ''')
-    all_rows = cursor.fetchall()
-    usageentries = len(all_rows)
-    if usageentries > 0:
-        report = ArtifactHtmlReport('Accounts_de')
-        report.start_artifact_report(report_folder, f'accounts_de_{uid}')
-        report.add_script()
-        data_headers = ('Last password entry','Name','Type')
-        data_list = []
-        for row in all_rows:
-            data_list.append((row[0], row[1], row[2]))
-        report.write_artifact_data_table(data_headers, data_list, folder)
-        report.end_artifact_report()
-        
-        tsvname = f'accounts de {uid}'
-        tsv(report_folder, data_headers, data_list, tsvname)
-        
-        tlactivity = f'Accounts DE {uid}'
-        timeline(report_folder, tlactivity, data_list, data_headers)
-    else:
-        logfunc(f'No accounts_de_{uid} data available')    
-    db.close()
+        self.artefact_reference = ''  # Description on what the artefact is.
+        self.path_filters = ['**/system_de/*/accounts_de.db']  # Collection of regex search filters to locate an artefact.
+        self.icon = ''  # feathricon for report.
+
+    def _processor(self) -> bool:
+
+        slash = '\\' if is_platform_windows() else '/'
+
+        # Filter for path xxx/yyy/system_ce/0
+        for file_found in self.files_found:
+            file_found = str(file_found)
+            parts = file_found.split(slash)
+            uid = parts[-2]
+            try:
+                uid_int = int(uid)
+                # Skip sbin/.magisk/mirror/data/system_de/0 , it should be duplicate data??
+                if file_found.find('{0}mirror{0}'.format(slash)) >= 0:
+                    continue
+                self._process_accounts_de(file_found, uid)
+            except ValueError:
+                    pass # uid was not a number
+
+        return True
+
+    def _process_accounts_de(self, folder, uid):
+
+        #Query to create report
+        db = open_sqlite_db_readonly(folder)
+        cursor = db.cursor()
+
+        #Query to create report
+        cursor.execute('''
+        SELECT
+            datetime(last_password_entry_time_millis_epoch / 1000, 'unixepoch') as 'last pass entry',
+            name,
+            type
+            FROM
+        accounts
+        ''')
+        all_rows = cursor.fetchall()
+        usageentries = len(all_rows)
+        if usageentries > 0:
+            report = ArtifactHtmlReport('Accounts_de')
+            report.start_artifact_report(self.report_folder, f'accounts_de_{uid}')
+            report.add_script()
+            data_headers = ('Last password entry','Name','Type')
+            data_list = []
+            for row in all_rows:
+                data_list.append((row[0], row[1], row[2]))
+            report.write_artifact_data_table(data_headers, data_list, folder)
+            report.end_artifact_report()
+
+            tsvname = f'accounts de {uid}'
+            tsv(self.report_folder, data_headers, data_list, tsvname)
+
+            tlactivity = f'Accounts DE {uid}'
+            timeline(self.report_folder, tlactivity, data_list, data_headers)
+        else:
+            logfunc(f'No accounts_de_{uid} data available')
+        db.close()
