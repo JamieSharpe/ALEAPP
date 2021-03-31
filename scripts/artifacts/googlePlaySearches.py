29bdc8a4c2593b043774f16edc82319a06674579
Jamie Sharpe <github@jamiesharpe.co.uk>
2021-03-31 11:00:00 +0100

    Converted googlePlaySearches.py to new plugin system.

--- scripts/artifacts/googlePlaySearches.py
+++ scripts/artifacts/googlePlaySearches.py
@@ -1,44 +1,61 @@
-import sqlite3
-import textwrap
-
+from scripts.ilapfuncs import timeline, open_sqlite_db_readonly
+from scripts.plugin_base import ArtefactPlugin
 from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows, open_sqlite_db_readonly
+from scripts.ilapfuncs import logfunc, tsv
 
-def get_googlePlaySearches(files_found, report_folder, seeker, wrap_text):
+
+class GooglePlaySearchesPlugin(ArtefactPlugin):
+    """
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.author = 'Unknown'
+        self.author_email = ''
+        self.author_url = ''
+
+        self.name = 'Google Play Searches'
+        self.description = ''
+
+        self.artefact_reference = ''  # Description on what the artefact is.
+        self.path_filters = ['**/com.android.vending/databases/suggestions.db*']  # Collection of regex search filters to locate an artefact.
+        self.icon = ''  # feathricon for report.
+
+    def _processor(self) -> bool:
     
-    file_found = str(files_found[0])
-    db = open_sqlite_db_readonly(file_found)
-    cursor = db.cursor()
-    cursor.execute('''
-    SELECT
-    datetime(date / 1000, "unixepoch"),
-    display1,
-    query
-    from suggestions
-    ''')
+        file_found = str(self.files_found[0])
+        db = open_sqlite_db_readonly(file_found)
+        cursor = db.cursor()
+        cursor.execute('''
+        SELECT
+        datetime(date / 1000, "unixepoch"),
+        display1,
+        query
+        from suggestions
+        ''')
 
-    all_rows = cursor.fetchall()
-    usageentries = len(all_rows)
-    if usageentries > 0:
-        report = ArtifactHtmlReport('Google Play Searches')
-        report.start_artifact_report(report_folder, 'Google Play Searches')
-        report.add_script()
-        data_headers = ('Timestamp','Display','query' ) # Don't remove the comma, that is required to make this a tuple as there is only 1 element
-        data_list = []
-        for row in all_rows:
-            data_list.append((row[0],row[1],row[2]))
+        all_rows = cursor.fetchall()
+        usageentries = len(all_rows)
+        if usageentries > 0:
+            report = ArtifactHtmlReport('Google Play Searches')
+            report.start_artifact_report(self.report_folder, 'Google Play Searches')
+            report.add_script()
+            data_headers = ('Timestamp','Display','query' ) # Don't remove the comma, that is required to make this a tuple as there is only 1 element
+            data_list = []
+            for row in all_rows:
+                data_list.append((row[0],row[1],row[2]))
 
-        report.write_artifact_data_table(data_headers, data_list, file_found)
-        report.end_artifact_report()
-        
-        tsvname = f'google play searches'
-        tsv(report_folder, data_headers, data_list, tsvname)
-        
-        tlactivity = f'Google Play Searches'
-        timeline(report_folder, tlactivity, data_list, data_headers)
-    else:
-        logfunc('No Google Play Searches data available')
-    
-    db.close()
-    return
+            report.write_artifact_data_table(data_headers, data_list, file_found)
+            report.end_artifact_report()
+
+            tsvname = f'google play searches'
+            tsv(self.report_folder, data_headers, data_list, tsvname)
+
+            tlactivity = f'Google Play Searches'
+            timeline(self.report_folder, tlactivity, data_list, data_headers)
+        else:
+            logfunc('No Google Play Searches data available')
+
+        db.close()
+        return True
 
