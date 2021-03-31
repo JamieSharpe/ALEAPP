16e70f789ac0cf1a1d2e565dcf1bbc6487a401e4
Mark McKinnon <github@jamiesharpe.co.uk>
2021-03-31 11:00:00 +0100

    Add new artifacts
    Add New artifacts
    
    (cherry picked from commit 02072b4123954d8377a8081be2793e857dadfde1)

--- scripts/artifacts/Zapya.py
+++ scripts/artifacts/Zapya.py
@@ -27,22 +27,32 @@ class ZapyaPlugin(ArtefactPlugin):
         db = open_sqlite_db_readonly(file_found)
         cursor = db.cursor()
         cursor.execute('''
-        SELECT device, name, direction, createtime, path, title FROM transfer
+        SELECT device, name, direction, createtime/1000, path, title FROM transfer
         ''')
 
         all_rows = cursor.fetchall()
         usageentries = len(all_rows)
         if usageentries > 0:
-            data_headers = ('Device','Name','direction','createtime','path', 'title') # Don't remove the comma, that is required to make this a tuple as there is only 1 element
+            data_headers = ('Device', 'Name', 'direction', 'fromid', 'toid', 'createtime', 'path', 'title')  # Don't remove the comma, that is required to make this a tuple as there is only 1 element
             data_list = []
             for row in all_rows:
-                data_list.append((row[0], row[1], row[2], row[3], row[4], row[5]))
+                from_id = ''
+                to_id = ''
+                if (row[2] == 1):
+                    direction = 'Outgoing'
+                    to_id = row[0]
+                else:
+                    direction = 'Incoming'
+                    from_id = row[0]
+
+                createtime = datetime.datetime.fromtimestamp(int(row[3])).strftime('%Y-%m-%d %H:%M:%S')
+                data_list.append((row[0], row[1], direction, from_id, to_id, createtime, row[4], row[5]))
 
             artifact_report.GenerateHtmlReport(self, file_found, data_headers, data_list)
 
             tsv(self.report_folder, data_headers, data_list, self.full_name())
 
-            timeline(self.report_folder, self.name, data_list, data_headers)
+            timeline(self.report_folder, self.full_name(), data_list, data_headers)
         else:
             logfunc('No Zapya data available')
 
