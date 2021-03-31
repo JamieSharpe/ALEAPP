46f1b2a3717c69461e1895ae50909db252731067
Jamie Sharpe <github@jamiesharpe.co.uk>
2021-03-31 11:00:00 +0100

    Converted VLC plugins to the new plugin system.

--- scripts/artifacts/vlcMedia.py
+++ scripts/artifacts/vlcMedia.py
@@ -1,58 +1,68 @@
-import sqlite3
-import os
+from scripts.ilapfuncs import timeline, open_sqlite_db_readonly
 
-from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import timeline, tsv, is_platform_windows, open_sqlite_db_readonly
+from scripts.plugin_base import ArtefactPlugin
+from scripts.ilapfuncs import logfunc, tsv
+from scripts import artifact_report
+
+class VlcMediaPlugin(ArtefactPlugin):
+    """
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.author = 'Unknown'
+        self.author_email = ''
+        self.author_url = ''
+
+        self.category = 'VLC'
+        self.name = 'Media List'
+        self.description = 'VLC Media List'
+
+        self.artefact_reference = ''  # Description on what the artefact is.
+        self.path_filters = ['*vlc_media.db*']  # Collection of regex search filters to locate an artefact.
+        self.icon = 'film'  # feathricon for report.
+
+    def _processor(self) -> bool:
+
+        for file_found in self.files_found:
+            file_found = str(file_found)
+
+            if file_found.endswith('vlc_media.db'):
+                break
+
+        db = open_sqlite_db_readonly(file_found)
+        cursor = db.cursor()
+        cursor.execute('''
+        SELECT
+        datetime(insertion_date, 'unixepoch'),
+        datetime(last_played_date,'unixepoch'),
+        filename,
+        path,
+        is_favorite
+        from Media
+        left join Folder
+        on Media.folder_id = Folder.id_folder
+        ''')
+
+        all_rows = cursor.fetchall()
+        usageentries = len(all_rows)
+        data_list = []
+
+        if usageentries > 0:
+            for row in all_rows:
+                data_list.append((row[0], row[1], row[2], row[3], row[4]))
+
+            data_headers = ('Insertion Date', 'Last Played Date', 'Filename', 'Path', 'Is Favorite?' )
+            artifact_report.GenerateHtmlReport(self, file_found, data_headers, data_list)
+
+            tsv(self.report_folder, data_headers, data_list, self.full_name())
+
+            timeline(self.report_folder, self.full_name(), data_list, data_headers)
+        else:
+            logfunc('No VLC Media data available')
+
+        db.close()
+
+        return True
 
 
-def get_vlcMedia(files_found, report_folder, seeker, wrap_text):
-    
-    for file_found in files_found:
-        file_found = str(file_found)
-        
-        if file_found.endswith('vlc_media.db'):
-            break
-            
-    db = open_sqlite_db_readonly(file_found)
-    cursor = db.cursor()
-    cursor.execute('''
-    SELECT
-    datetime(insertion_date, 'unixepoch'),
-    datetime(last_played_date,'unixepoch'),
-    filename,
-    path,
-    is_favorite
-    from Media
-    left join Folder
-    on Media.folder_id = Folder.id_folder
-    ''')
-
-    all_rows = cursor.fetchall()
-    usageentries = len(all_rows)
-    data_list = []  
-    
-    if usageentries > 0:
-        for row in all_rows:
-            data_list.append((row[0], row[1], row[2], row[3], row[4]))
-
-        description = 'VLC Media List'
-        report = ArtifactHtmlReport('VLC Media List')
-        report.start_artifact_report(report_folder, 'VLC Media List', description)
-        report.add_script()
-        data_headers = ('Insertion Date', 'Last Played Date', 'Filename', 'Path', 'Is Favorite?' )
-        report.write_artifact_data_table(data_headers, data_list, file_found)
-        report.end_artifact_report()
-        
-        tsvname = 'VLC Media'
-        tsv(report_folder, data_headers, data_list, tsvname)
-        
-        tlactivity = 'VLC Media'
-        timeline(report_folder, tlactivity, data_list, data_headers)
-    else:
-        logfunc('No VLC Media data available')
-    
-    db.close()
-    return 
-
-        
-        
--- scripts/artifacts/vlcThumbs.py
+++ scripts/artifacts/vlcThumbs.py
@@ -1,36 +1,42 @@
 import os
 import shutil
 
-from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import timeline, tsv, is_platform_windows, open_sqlite_db_readonly
+from scripts.plugin_base import ArtefactPlugin
+from scripts.ilapfuncs import tsv
+from scripts import artifact_report
 
+class VlcThumbsPlugin(ArtefactPlugin):
+    """
+    """
 
-def get_vlcThumbs(files_found, report_folder, seeker, wrap_text):
-    data_list = []
-    for file_found in files_found:
-        file_found = str(file_found)
-        
-        data_file_real_path = file_found
-        shutil.copy2(data_file_real_path, report_folder)
-        data_file_name = os.path.basename(data_file_real_path)
-        thumb = f'<img src="{report_folder}/{data_file_name}"></img>'
-        
-        data_list.append((data_file_name, thumb))
-    
-    path_to_files = os.path.dirname(data_file_real_path)
-    
-    description = 'VLC Thumbnails'
-    report = ArtifactHtmlReport('VLC Thumbnails')
-    report.start_artifact_report(report_folder, 'VLC Thumbnails', description)
-    report.add_script()
-    data_headers = ('Filename', 'Thumbnail' )
-    report.write_artifact_data_table(data_headers, data_list, path_to_files, html_escape=False)
-    report.end_artifact_report()
-    
-    tsvname = 'VLC Thumbnails'
-    tsv(report_folder, data_headers, data_list, tsvname)
-        
+    def __init__(self):
+        super().__init__()
+        self.author = 'Unknown'
+        self.author_email = ''
+        self.author_url = ''
 
+        self.category = 'VLC'
+        self.name = 'Thumbnails'
+        self.description = 'VLC thumbnail icons.'
 
-        
-        
+        self.artefact_reference = ''  # Description on what the artefact is.
+        self.path_filters = ['*/org.videolan.vlc/files/medialib/*.jpg']  # Collection of regex search filters to locate an artefact.
+        self.icon = 'image'  # feathricon for report.
+
+    def _processor(self) -> bool:
+
+        data_list = []
+        for file_found in self.files_found:
+
+            data_file_real_path = file_found
+            shutil.copy2(data_file_real_path, self.report_folder)
+            data_file_name = os.path.basename(data_file_real_path)
+            thumb = f'<img src="{self.report_folder}/{data_file_name}"></img>'
+
+            data_list.append((data_file_name, thumb))
+
+        path_to_files = os.path.dirname(file_found)
+        data_headers = ('Filename', 'Thumbnail')
+        artifact_report.GenerateHtmlReport(self, path_to_files, data_headers, data_list)
+
+        tsv(self.report_folder, data_headers, data_list, self.full_name())
