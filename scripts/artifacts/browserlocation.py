952c3c229d88e3911115fb338b96075629b9315b
Jamie Sharpe <github@jamiesharpe.co.uk>
2021-03-31 11:00:00 +0100

    Updated browserlocation.py to enw plugin system.

--- scripts/artifacts/browserlocation.py
+++ scripts/artifacts/browserlocation.py
@@ -1,51 +1,68 @@
-import sqlite3
 import datetime
 
-from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows, open_sqlite_db_readonly
+from scripts.ilapfuncs import open_sqlite_db_readonly
+from scripts.plugin_base import ArtefactPlugin
+from scripts.ilapfuncs import logfunc, tsv
+from scripts import artifact_report
 
-def get_browserlocation(files_found, report_folder, seeker, wrap_text):
 
-    source_file = ''
+class BrowserLocationPlugin(ArtefactPlugin):
+    """
+    """
 
-    for file_found in files_found:
-        file_found = str(file_found)
-        
-        if file_found.endswith('-db'):
-            source_file = file_found.replace(seeker.directory, '')
-            continue
-  
-        source_file = file_found.replace(seeker.directory, '')
-        
-        db = open_sqlite_db_readonly(file_found)
-        cursor = db.cursor()
-        try:
-            cursor.execute('''
-            SELECT timestamp/1000, latitude, longitude, accuracy FROM CachedPosition;
-            ''')
+    def __init__(self):
+        super().__init__()
+        self.author = 'Unknown'
+        self.author_email = ''
+        self.author_url = ''
 
-            all_rows = cursor.fetchall()
-            usageentries = len(all_rows)
-        except:
-            usageentries = 0
-            
-        if usageentries > 0:
-            report = ArtifactHtmlReport('Browser Locations')
-            report.start_artifact_report(report_folder, 'Browser Locations')
-            report.add_script()
-            data_headers = ('timestamp','latitude', 'longitude', 'accuracy') # Don't remove the comma, that is required to make this a tuple as there is only 1 element
-            data_list = []
-            for row in all_rows:
-                timestamp = datetime.datetime.fromtimestamp(int(row[0])).strftime('%Y-%m-%d %H:%M:%S') 
-                data_list.append((timestamp, row[1], row[2], row[3]))
+        self.category = 'Browser'
+        self.name = 'Locations'
+        self.description = ''
 
-            report.write_artifact_data_table(data_headers, data_list, file_found)
-            report.end_artifact_report()
-            
-            tsvname = f'Browser Locations'
-            tsv(report_folder, data_headers, data_list, tsvname, source_file)
-            
-        else:
-            logfunc('No Browser Locations found')
-            
-        db.close()
+        self.artefact_reference = ''  # Description on what the artefact is.
+        self.path_filters = ['**/com.android.browser/app_geolocation/CachedGeoposition.db']  # Collection of regex search filters to locate an artefact.
+        self.icon = 'map-pin'  # feathricon for report.
+
+    def _processor(self) -> bool:
+
+        source_file = ''
+
+        for file_found in self.files_found:
+            file_found = str(file_found)
+
+            if file_found.endswith('-db'):
+                source_file = file_found.replace(self.seeker.directory, '')
+                continue
+
+            source_file = file_found.replace(self.seeker.directory, '')
+
+            db = open_sqlite_db_readonly(file_found)
+            cursor = db.cursor()
+            try:
+                cursor.execute('''
+                SELECT timestamp/1000, latitude, longitude, accuracy FROM CachedPosition;
+                ''')
+
+                all_rows = cursor.fetchall()
+                usageentries = len(all_rows)
+            except:
+                usageentries = 0
+
+            if usageentries > 0:
+                data_headers = ('timestamp','latitude', 'longitude', 'accuracy') # Don't remove the comma, that is required to make this a tuple as there is only 1 element
+                data_list = []
+                for row in all_rows:
+                    timestamp = datetime.datetime.fromtimestamp(int(row[0])).strftime('%Y-%m-%d %H:%M:%S')
+                    data_list.append((timestamp, row[1], row[2], row[3]))
+
+                artifact_report.GenerateHtmlReport(self, file_found, data_headers, data_list)
+
+                tsv(self.report_folder, data_headers, data_list, self.full_name(), source_file)
+
+            else:
+                logfunc('No Browser Locations found')
+
+            db.close()
+
+        return True
