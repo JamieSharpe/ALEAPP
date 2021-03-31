2402c3801d49e068d1e87521f31901a833cb59d6
Jamie Sharpe <github@jamiesharpe.co.uk>
2021-03-31 11:00:00 +0100

    Added packageGplinks.py from commit: 9a761d70a7387faa32c3c9fe89eea61b915b9ac4
    and converted to new plugin system.
    
    (Cherry Pick)

--- scripts/artifacts/packageGplinks.py
+++ scripts/artifacts/packageGplinks.py
@@ -1,34 +1,50 @@
+from scripts.plugin_base import ArtefactPlugin
+from scripts.ilapfuncs import logfunc, tsv
+from scripts import artifact_report
 
 
-from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, tsv, timeline, open_sqlite_db_readonly
+class PackageGplinksPlugin(ArtefactPlugin):
+    """
+    """
 
-def get_packageGplinks(files_found, report_folder, seeker, wrap_text):
-    data_list =[]
-    
-    for file_found in files_found:
-        if 'sbin' not in file_found:
-            file_found = str(file_found)
-    
-    with open(file_found) as data:
-        values = data.readlines()
-        
-    for x in values:
-        bundleid = x.split(' ', 1)
-        url = f'<a href="https://play.google.com/store/apps/details?id={bundleid[0]}" target="_blank"><font color="blue">https://play.google.com/store/apps/details?id={bundleid[0]}</font></a>'
-        data_list.append((bundleid[0], url))
+    def __init__(self):
+        super().__init__()
+        self.author = 'Unknown'
+        self.author_email = ''
+        self.author_url = ''
 
-    usageentries = len(data_list)
-    if usageentries > 0:
-        report = ArtifactHtmlReport('Google Play Links for Apps')
-        report.start_artifact_report(report_folder, 'Google Play Links for Apps')
-        report.add_script()
-        data_headers = ('Bundle ID', 'Possible Google Play Store Link')
-        report.write_artifact_data_table(data_headers, data_list, file_found, html_escape=False)
-        report.end_artifact_report()
-        
-        tsvname = f'Google Play Links for Apps'
-        tsv(report_folder, data_headers, data_list, tsvname)
-        
-    else:
-        logfunc('No Google Play Links for Apps data available')
+        self.category = 'Google Play'
+        self.name = 'Links for Apps'
+        self.description = ''
+
+        self.artefact_reference = ''  # Description on what the artefact is.
+        self.path_filters = ['*/system/packages.list']  # Collection of regex search filters to locate an artefact.
+        self.icon = 'package'  # feathricon for report.
+
+    def _processor(self) -> bool:
+        data_list = []
+
+        for file_found in self.files_found:
+            if 'sbin' not in file_found:
+                file_found = str(file_found)
+
+            with open(file_found) as data:
+                values = data.readlines()
+
+            for x in values:
+                bundleid = x.split(' ', 1)
+                url = f'<a href="https://play.google.com/store/apps/details?id={bundleid[0]}" target="_blank"><font color="blue">https://play.google.com/store/apps/details?id={bundleid[0]}</font></a>'
+                data_list.append((bundleid[0], url))
+
+            usageentries = len(data_list)
+            if usageentries > 0:
+                data_headers = ('Bundle ID', 'Possible Google Play Store Link')
+
+                artifact_report.GenerateHtmlReport(self, file_found, data_headers, data_list, allow_html = True)
+
+                tsv(self.report_folder, data_headers, data_list, self.full_name())
+
+            else:
+                logfunc('No Google Play Links for Apps data available')
+
+        return True
